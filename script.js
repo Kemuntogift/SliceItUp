7bbd0d1a38cab7976e0dd7cc7a44ec3acdcce00e
Kemunto Gift <giftoyugi@gmail.com>
2022-03-19 20:10:15 +0300

    add variables for flavour, size, crust, topping, quantity and total

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 $(document).ready(function() {
             $("#submit").click(function(event) {
+                        var flavour = parseFloat(document.getElementById('flavour').value);
+                        var size = parseFloat(document.getElementById('size').value);
+                        var crust = parseFloat(document.getElementById('crust').value);
+                        var topping = parseFloat(document.getElementById('extra-topping').value);
+                        var quantity = parseFloat(document.getElementById('number').value);
+                        var total = (flavour + size + crust + topping + quantity + Amount);
