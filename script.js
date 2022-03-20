28491c6bcc8900794580e7cbe4abb672dab32765
Kemunto Gift <giftoyugi@gmail.com>
2022-03-20 11:13:30 +0300

    add footer section

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 $(document).ready(function() {
-            $("#submit").click(function(event) {
-                        var flavour = parseFloat(document.getElementById('flavour').value);
-                        var size = parseFloat(document.getElementById('size').value);
-                        var crust = parseFloat(document.getElementById('crust').value);
-                        var topping = parseFloat(document.getElementById('extra-topping').value);
-                        var quantity = parseFloat(document.getElementById('number').value);
-                        var total = (flavour + size + crust + topping + quantity + Amount);
+    $("#submit").click(function(event) {
+        var flavour = parseFloat(document.getElementById("flavour").value);
+        var size = parseFloat(document.getElementById("size").value);
+        var crust = parseFloat(document.getElementById("crust").value);
+        var topping = parseFloat(document.getElementById("extra-topping").value);
+        var quantity = parseFloat(document.getElementById("number").value);
+        var total = flavour + size + crust + topping + quantity + Amount;
 
-                        document.getElementById("getsize").innerHTML = Size;
-                        document.getElementById("getcrust").innerHTML = Crust;
-                        document.getElementById("gettopping").innerHTML = Topping;
-                        document.getElementById("sum").innerHTML = total;
+        document.getElementById("getsize").innerHTML = size;
+        document.getElementById("getcrust").innerHTML = crust;
+        document.getElementById("gettopping").innerHTML = topping;
+        document.getElementById("sum").innerHTML = total;
+        $(".the-order").show();
+        event.preventDefault();
+    });
+});
